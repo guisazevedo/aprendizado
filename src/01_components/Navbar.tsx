410d0f72cd83d7d794431475e281c74a6f052220
Guilherme de Azevedo <guisazevedo@gmail.com>
2023-05-31 16:08:46 +0200

    navbar v.1

---
+++ src/01_components/Navbar.tsx
@@ -0,0 +1,61 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const menu_items = [
+    { title: "Principal", link: "/" },
+    { title: "Quem Somos", link: "/about" },
+    { title: "Contato", link: "/contact" },
+    { title: "FAQ", link: "/faq" },
+  ];
+
+  const log_items = [
+    { title: "Entrar", link: "/login" },
+    { title: "Registro", link: "/register" },
+  ];
+
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <div className="container-fluid px-5">
+        <Link className="navbar-brand" to="/">
+          <span>
+            <i className="bi bi-compass h1"></i>
+          </span>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav me-auto mb-2 mb-md-0">
+            {menu_items.map((item) => (
+              <li className="nav-item px-2">
+                <Link className="nav-link active" to={item.link}>
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="row">
+          {log_items.map((item) => (
+            <div className="col">
+              <button type="button" className="btn btn-outline-light px-2">
+                <span className="p-3">{item.title}</span>
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/03_pages/Landing_page.tsx
+++ src/03_pages/Landing_page.tsx
@@ -1,9 +1,15 @@
+import Navbar from "../01_components/Navbar";
+
 const Landing_page = () => {
   return (
     <>
-      <div>Landing_page</div>
+      <Navbar />
     </>
   );
 };
 
 export default Landing_page;
+
+// TODO => Navbar
+// TODO => Body
+// TODO => Footer
