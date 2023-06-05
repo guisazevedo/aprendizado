457dba6e1ba6fe81a912457d2b70ba9776f68fe7
Guilherme de Azevedo <guisazevedo@gmail.com>
2023-06-05 15:49:37 +0200

    implement about_header_block

---
+++ src/02_blocks/About_header_block.tsx
@@ -0,0 +1,17 @@
+import HeaderText from "../01_components/HeaderText";
+
+// inputs for the HeaderText component
+const header = "Quem somos";
+const sub_header = "Nos somos....";
+const text =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat error voluptatem voluptates dolor est debitis quae incidunt quas ipsa et eveniet nulla dignissimos dolorum numquam soluta dolore, possimus nisi corrupti.";
+
+const About_header_block = () => {
+  return (
+    <>
+      <HeaderText header={header} sub_header={sub_header} text={text} />
+    </>
+  );
+};
+
+export default About_header_block;
--- src/03_pages/About_page.tsx
+++ src/03_pages/About_page.tsx
@@ -1,23 +1,15 @@
 import Footer from "../01_components/Footer";
-import HeaderText from "../01_components/HeaderText";
+import About_header_block from "../02_blocks/About_header_block";
 import Navbar from "../01_components/Navbar";
-
-// inputs for the HeaderText component
-const header = "Quem somos";
-const sub_header = "Nos somos....";
-const text =
-  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat error voluptatem voluptates dolor est debitis quae incidunt quas ipsa et eveniet nulla dignissimos dolorum numquam soluta dolore, possimus nisi corrupti.";
 
 const About_page = () => {
   return (
     <>
       <Navbar />
       <div className="py-5"></div>
-      <div>
-        <HeaderText header={header} sub_header={sub_header} text={text} />
-
-        <h1>Discutir o que fazer</h1>
-      </div>
+      <About_header_block />
+      <div className="p-5"></div>
+      <h1 className="text-center">Discutir o que fazer</h1>
       <div className="p-5"></div>
       <Footer />
     </>
@@ -27,4 +19,3 @@ const About_page = () => {
 export default About_page;
 
 // TODO => discutir o que fazer
-// TODO => break headerText from About_page in block
